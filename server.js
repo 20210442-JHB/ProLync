ad48c632fa93713681250f07cc1492af6fa54858
Min <ckalstkd1019@naver.com>
2026-06-03 20:19:54 +0900

    Fix: 배포를 위해 process.env.PORT 적용

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 const app  = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
